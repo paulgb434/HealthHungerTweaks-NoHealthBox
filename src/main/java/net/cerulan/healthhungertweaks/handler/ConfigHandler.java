313bcd068ca8950bfad8f0b649ba33d1f6d3654c
paulgb434 <32685848+paulgb434@users.noreply.github.com>
2019-06-08 04:39:31 -0400

    Update ConfigHandler.java

--- src/main/java/net/cerulan/healthhungertweaks/handler/ConfigHandler.java
+++ src/main/java/net/cerulan/healthhungertweaks/handler/ConfigHandler.java
@@ -14,7 +14,6 @@ public class ConfigHandler {
 	private int cdX, cdY;
 	private boolean screenDarkenWhenInjure;
 	
-	private boolean disableRegularRegen;
 	private int delayUntilStart;
 	private int delayBetweenTicks;
 	private int minimumHunger;
@@ -29,8 +28,6 @@ public class ConfigHandler {
 		screenDarkenWhenInjure = config.get("client", "screenDarken", true, "Whether or not the screen should darken when injured").getBoolean();
 		
 		exhaustionModifier = config.get("exhaustion", "exhaustionModifier", 1.0, "An exhaustion modifier that will be multiplied to the default maximum exhaustion. Higher values mean slower food drain.").getDouble();
-		
-		disableRegularRegen = config.get("mending", "disableRegularRegen", true, "Toggles whether regular regen (from food) should be disabled, and players must use health kits. Recommended if food is made easier. (This in itself also makes food easier as saturation will not be consumed to restore health).").getBoolean();
 		delayUntilStart = config.get("mending", "delayUntilStart", 200, "The delay (in ticks) before a player will begin to regenerate health. Negative values disable this functionality, forcing players to rely solely on health kits or potions.").getInt();
 		delayBetweenTicks = config.get("mending", "delayBetweenTicks", 10, "The delay (in ticks) between each regeneration.").getInt();
 		
@@ -48,8 +45,6 @@ public class ConfigHandler {
 	}
 	
 	public double getExhaustionModifier() { return exhaustionModifier; }
-	
-	public boolean shouldDisableRegularRegen() { return disableRegularRegen; }
 	
 	public boolean shouldScreenDarkenWhenInjured() { return this.screenDarkenWhenInjure; }
 
